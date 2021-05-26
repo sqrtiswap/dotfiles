eb33927e7e1866624d065b1ea9065e33a64da939
Alexander Möller <alexander.moeller@detmold.com>
2021-05-27 00:34:49 +0200

    termbar.sh: Change some colours. Add an array for use in battery(). Make functions output same length commands/variables regardless of the output of the if statements so that the output occupies the same space; For that tend to output into a variable instead of echoing.

--- termbar.sh
+++ termbar.sh
@@ -7,16 +7,16 @@ _rset="\033[0m"
 _hide="\033[2m"		# make colour less vibrant
 _line="\033[9m"		# line through the output
 _back="\033[22m"	# reverse _hide
-_crit="\033[31m"	# red
+_crit="\033[31m"	# dark red
 _good="\033[32m"	# green
 _warn="\033[33m"	# yellow
 _alrt="\033[34m"	# blue
 _norm="\033[39m"	# white
 
-grey="${_norm}${_hide}"
+_grey="${_norm}${_hide}"
 pipe="${_back}${_norm} |"
 
-red="\033[31m"
+_dred="\033[31m"	# dark red
 
 #black="\033[30m"
 #magenta="\033[35m"
@@ -32,25 +32,28 @@ red="\033[31m"
 #brightcyan="\033[96m"
 #brightwhite="\033[97m"
 
-set -A bat "${_good}" "${_warn}" "${_crit}" "${grey}" "${_alrt}"
-set -A net "${_rset}${_good}" "${grey}" "${_crit}"
+set -A bat "${_good}" "${_warn}" "${_crit}" "${_grey}" "${_alrt}"
+set -A eta " ($(apm -m))m)" ""
+#set -A eta "($(printf "%3s" "$(apm -m)")m)" ""
+set -A net "${_rset}${_good}" "${_grey}" "${_crit}"
 set -A nic "em0" "iwm0" "ure0"
-set -A vol "${_good}" "${grey}"
+set -A vol "${_good}" "${_grey}"
 
 battery() {
 # apm -b: 0 high, 1 low, 2 critical, 3 charging, 4 absent, 255 unknown
 	#_adapter=$(apm -a)
 	_status=$(sysctl -n hw.sensors.acpiac0.indicator0 | grep -c On)
-	_batpercent=$(printf "%3s" "$(apm -l)")
-	_battime=$(printf "%3s" "$(apm -m)")
+	#_batpercent=$(printf "%3s" "$(apm -l)")
+	_batpercent="$(apm -l)"
+	#_battime=$(printf "%3s" "$(apm -m)")
 	[[ ${_status} -eq 1 ]] \
-		&& echo -n "${bat[0]}AC: ${bat[$(apm -b)]}${_back}${_batpercent}% (---m)${pipe}" \
-		|| echo -n "${bat[3]}AC: ${bat[$(apm -b)]}${_back}${_batpercent}% (${_battime}m)${pipe}"
+		&& echo -n "${bat[0]}AC: ${bat[$(apm -b)]}${_back}${_batpercent}%${eta[1]}${pipe}" \
+		|| echo -n "${bat[3]}AC: ${bat[$(apm -b)]}${_back}${_batpercent}%${eta[0]}${pipe}"
 }
 
 calendar() {
-	sep="${grey}&${_back}${red}"
-	echo -n "${_norm}$(date "+${red}%A, %d %B %Y ${sep} %T %Z %z ${sep} %V ${sep} %j")${pipe}"
+	sep="${_grey}&${_back}${_dred}"
+	echo -n "${_norm}$(date "+${_dred}%A, %d %B %Y ${sep} %T %Z %z ${sep} %V ${sep} %j")${pipe}"
 }
 
 cpu() {
@@ -58,7 +61,6 @@ cpu() {
 	_cputemp=$(sysctl -n hw.sensors.cpu0.temp0 | cut -d '.' -f 1)
 	_cpuspeed=$(printf "%4s" "$(sysctl -n hw.cpuspeed)")
 	_cpuperf=$(printf "%4s" "$(sysctl -n hw.setperf)%")
-	#echo -n "${_cpuload}% ${_cputemp}°C ${_cpuspeed} MHz @ ${_cpuperf}${pipe}"
 	if [ ${_cpuload} -ge 90 ] ; then
 		echo -n "${_crit}${_cpuload}% "
 	elif [ ${_cpuload} -ge 60 ] ; then
@@ -73,11 +75,11 @@ cpu() {
 	else
 		echo -n "${_good}${_cputemp}°C"
 	fi
-	echo -n " ${grey}${_cpuspeed} MHz @ ${_cpuperf}${_rset}${pipe}"
+	echo -n " ${_grey}${_cpuspeed} MHz @ ${_cpuperf}${_rset}${pipe}"
 }
 
 group() {
-	echo -n "${grey}$(xprop -root 32c '\t$0' _NET_CURRENT_DESKTOP | cut -f 2)${_rset}"
+	echo -n "${_grey}$(xprop -root 32c '\t$0' _NET_CURRENT_DESKTOP | cut -f 2)${_rset}"
 }
 
 load() {
@@ -108,70 +110,71 @@ music() {
 	_playstate=$(mpc | grep -c playing)
 	_pausestate=$(mpc | grep -c paused)
 	if [[ ${_playstate} -eq 1 ]] ; then
-		_music=$(printf "%24s" "${grey}${_position}mpd: play")
+		_music="${_grey}${_position}"
 	elif [[ ${_pausestate} -eq 1 ]] ; then
-		_music=$(printf "%24s" "${grey}${_position}mpd: pause")
+		_music="${_grey}${_position}"
 	else
-		_music=$(printf "%24s" "${grey}0/0 (0:00/0:00) -------")
+		_music="${_grey}0/0 (0:00/0:00) -------"
 	fi
-	echo -n "${_music}${_back}"
+	echo -n "${_music}$(mpc -f %artist% current)${_back}"
 }
 
 network() {
 	_lanstat=$(ifconfig "${nic[0]}" | grep -c 'status: active')
 	_wlanstat=$(ifconfig "${nic[1]}" | grep -c 'status: active')
-	#_wlanid=$(ifconfig ${nic[1]} | awk '/(nwid|join)/ { print $3 }')
-	#_wlansig=$(ifconfig ${nic[1]} | awk 'match($0, /.[0-9]%/) { print substr($0, RSTART, RLENGTH) }')
-	_wlan=$(printf "%13s" "$(ifconfig "${nic[1]}" | awk '/ieee80211:/ { print $3 "(" $8 ")" }')")
+	_wlan="$(ifconfig "${nic[1]}" | awk '/ieee80211:/ { print $3 "(" $8 ")" }')"
 	_hublanexist=$(ifconfig | grep -c "${nic[2]}:")
 	if [[ ${_lanstat} -ne 1 && ${_wlanstat} -ne 1 ]] ; then
-		if [[ ${_hublanexist} = 0 ]] ; then
-			_hubnet=$(printf "%24s" "${net[2]}no network")
+		if [[ ${_hublanexist} -eq 0 ]] ; then
+			_netstate="${net[2]}no network"
 		else
 			_hublanup=$(ifconfig "${nic[2]}" | grep -c UP)
 			_hublanstat=$(ifconfig "${nic[2]}" | grep -c inet)
 			[[ ${_hublanup} -eq 1 && ${_hublanstat} -gt 0 ]] \
-				&& _hubnet="         ${net[1]}${nic[0]} ${nic[1]}${net[0]} ${nic[2]}" \
-				|| _hubnet=$(printf "%24s" "${net[2]}no network")
+				&& _netstate="${net[1]}${nic[0]} ${nic[1]}${net[0]} ${nic[2]}" \
+				|| _netstate="${net[2]}no network"
 		fi
-		echo -n "${_hubnet}"
 	else
 		[[ ${_lanstat} -eq 1 ]] \
-			&& echo -n "${net[0]}${nic[0]} " \
-			|| echo -n "${net[1]}${nic[0]} "
+			&& _lanstate="${net[0]}${nic[0]} " \
+			|| _lanstate="${net[1]}${nic[0]} "
 		[[ ${_wlanstat} -eq 1 ]] \
-			&& echo -n "${net[0]}${_wlan}" \
-			|| echo -n "${net[1]}${nic[1]}"
+			&& _wlanstate="${net[0]}${_wlan}" \
+			|| _wlanstate="${net[1]}${nic[1]}"
 		if [[ ${_hublanexist} -eq 1 ]] ; then
 			_hublanup=$(ifconfig "${nic[2]}" | grep -c UP)
 			_hublanstat=$(ifconfig "${nic[2]}" | grep -c inet)
 			[[ ${_hublanup} -eq 1 && ${_hublanstat} -gt 0 ]] \
-				&& echo -n " ${net[0]}${nic[2]}" \
-				|| echo -n " ${net[1]}${nic[2]}"
+				&& _hublanstate=" ${net[0]}${nic[2]}" \
+				|| _hublanstate=" ${net[1]}${nic[2]}"
 		else
-			echo -n " ${_line}${grey}${nic[2]}"
+			_hublanstate=" ${_line}${_grey}${nic[2]}"
 		fi
+		_netstate="${_lanstate}${_wlanstate}${_hublanstate}"
 	fi
-	echo -n "${_rset}${pipe}"
+	echo -n "${_netstate}${_rset}${pipe}"
 }
 
 snapshot() {
-	_snapshot=$(printf "%16s" "$(awk -F "( |:)" 'NR == 1 { print $2" "$4 }' /etc/motd)")
+	#_snapshot=$(printf "%16s" "$(awk -F "( |:)" 'NR == 1 { print $2" "$4 }' /etc/motd)")
+	_snapshot=$(awk -F "( |:)" 'NR == 1 { print $2" "$4 }' /etc/motd)
 	_kernel=$(uname -v | grep -c 'GENERIC.MP')
 	[[ ${_kernel} -eq 1 ]] \
-		&& echo -n "${grey}${_snapshot}${pipe}" \
+		&& echo -n "${_grey}${_snapshot}${pipe}" \
 		|| echo -n "${_alrt}${_snapshot}${pipe}"
 }
 
 tasks() {
-	_today=$(printf "%2s" "$(grep -c due:"$(date +%Y-%m-%d)" ~/todo/todo.txt)")
+	#_today=$(printf "%2s" "$(grep -c due:"$(date +%Y-%m-%d)" ~/todo/todo.txt)")
+	_today=$(grep -c due:"$(date +%Y-%m-%d)" ~/todo/todo.txt)
 	[[ ${_today} != 0 ]] \
-		&& echo -n "${red}${_today} " \
-		|| echo -n "${grey}${_today} "
-	_urgent=$(printf "%2s" "$(grep -c '_urgent' ~/todo/todo.txt)")
+		&& echo -n "${_dred}${_today} " \
+		|| echo -n "${_grey}${_today} "
+	#_urgent=$(printf "%2s" "$(grep -c '_urgent' ~/todo/todo.txt)")
+	_urgent=$(grep -c '_urgent' ~/todo/todo.txt)
 	[[ ${_urgent} != 0 ]] \
-		&& echo -n "${_back}${red}${_urgent}${pipe}" \
-		|| echo -n "${grey}${_urgent}${pipe}"
+		&& echo -n "${_back}${_dred}${_urgent}${pipe}" \
+		|| echo -n "${_grey}${_urgent}${pipe}"
 }
 
 volume() {
@@ -198,9 +201,10 @@ while true; do
 	tput cup 1 0
 	_l=" $(calendar) $(tasks) $(music)"
 	_r="| $(volume) $(network) $(cpu) $(memory) $(load) $(battery) $(snapshot) $(group)"
-	printf "%-215.215s\r" "$_l"
-	tput cup 1 92
-	printf "%300.300s" "$_r"
+	printf "%-240.240s\r" "$_l"
+	#printf "%-300.300s\r" "$_l"
+	tput cup 1 108
+	printf "%280.280s" "$_r"
 	sleep 1
 done
 

c254f53e7830cc17d61e86040bc709cf29b13ab4
Alexander Möller <alexander.moeller@detmold.com>
2021-06-17 09:34:08 +0200

    Further colour correction in termbar.sh by increasing printf characters.

--- termbar.sh
+++ termbar.sh
@@ -180,7 +180,7 @@ while true; do
 	tput cup 1 0
 	_l=" $(calendar) $(tasks) $(network) $(battery) $(music)"
 	_r="$(volume) $(cpu) $(memory) $(load) $(snapshot) $(group)"
-	printf "%-325.325s\r" "$_l"
+	printf "%-340.340s\r" "$_l"
 	tput cup 1 138
 	printf "%185.185s" "$_r"
 	sleep 5

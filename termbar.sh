3d14dd5b33a6881fe9ae9d69d0b426134d8e05a2
Alexander Möller <alexander.moeller@detmold.com>
2021-12-22 15:32:02 +0100

    termbar.sh: - Comment out unused colours. - Simplify some of the arrays, especially concerning grey colouring. - Simplify checking whether we are connected to ac in battery(). This should also ease the load since it droppes one grep(1) call. - cpu(): Reorder some stuff around. Also simplify _cpuload while dropping a printf(1), should also ease load. Change colouring of _cpuload. Don't get _cpuspeed any more since it's redundant through _cpuperf. - Add function fan() to print the rotations per minute. - Uncomment unused functions to make them better accessible and more easy to use again in the future. - Make load() work with loads > 10. - Make memory() resistant to errors with regards to top(1) output. - Make network() colouring more robust by calling _rset. - tasks() now only greps once to get the sum total of tasks with either today's date or the +urgent tag. Therefore only print one number. - Make volume() also reference an array for the colouring instead of calling test(1). - Change timeout from 1 second to 5 seconds. - Reflect above changes in the while loop.

--- termbar.sh
+++ termbar.sh
@@ -17,19 +17,21 @@ _grey="${_norm}${_hide}"
 pipe="${_back}${_norm} |"
 
 _dred="\033[31m"	# dark red
+#_pink="\033[35m"	# magenta
+#_teal="\033[36m"	# teal
 
 set -A bat "${_good}" "${_warn}" "${_crit}" "${_hide}" "${_alrt}"
-set -A net "${_rset}${_good}" "${_grey}" "${_crit}" "${_rset}${_hide}${_line}"
+set -A net "${_good}" "${_hide}" "${_crit}" "${_hide}${_line}"
 set -A nic "em0" "iwm0" "ure0"
-set -A vol "${_good}" "${_grey}"
+set -A vol "${_good}" "${_crit}"
 
 battery() {
 # apm -b: 0 high, 1 low, 2 critical, 3 charging, 4 absent, 255 unknown
 	#_adapter=$(apm -a)
-	_status=$(sysctl -n hw.sensors.acpiac0.indicator0 | grep -c On)
+	_status=$(sysctl -n hw.sensors.acpiac0.indicator0)
 	_batpercent="$(apm -l)%"
 	_eta="($(apm -m)m)"
-	[[ ${_status} -eq 1 ]] \
+	[[ ${_status} = "On (power supply)" ]] \
 		&& echo -n "${bat[0]}AC: ${bat[$(apm -b)]}${_back}${_batpercent}${pipe}" \
 		|| echo -n "${bat[3]}AC: ${bat[$(apm -b)]}${_back}${_batpercent} ${_eta}${pipe}"
 }
@@ -40,17 +42,17 @@ calendar() {
 }
 
 cpu() {
-	_cpuload=$(printf "%2s" "$((100-$(iostat -C | awk -F " " 'NR == 3 { print $6 }')))")
-	_cputemp=$(sysctl -n hw.sensors.cpu0.temp0 | cut -d '.' -f 1)
-	_cpuspeed=$(printf "%4s" "$(sysctl -n hw.cpuspeed)")
-	_cpuperf=$(printf "%4s" "$(sysctl -n hw.setperf)%")
-	if [ "${_cpuload}" -ge 90 ] ; then
+	#_cpuload=$(printf "%2s" "$((100-$(iostat -C | awk -F " " 'NR == 3 { print $6 }')))")
+	_cpuload=$((100-$(iostat -C | awk -F " " 'NR == 3 { print $6 }')))
+	if [ "${_cpuload}" -ge 50 ] ; then
 		echo -n "${_crit}${_cpuload}% "
-	elif [ "${_cpuload}" -ge 60 ] ; then
+	elif [ "${_cpuload}" -ge 25 ] ; then
 		echo -n "${_warn}${_cpuload}% "
 	else
 		echo -n "${_good}${_cpuload}% "
 	fi
+
+	_cputemp=$(sysctl -n hw.sensors.cpu0.temp0 | cut -d '.' -f 1)
 	if [[ ${_cputemp} -gt 70 ]] ; then
 		echo -n "${_crit}${_cputemp}°C"
 	elif [[ ${_cputemp} -gt 60 ]] ; then
@@ -58,25 +60,37 @@ cpu() {
 	else
 		echo -n "${_good}${_cputemp}°C"
 	fi
-	echo -n " ${_grey}${_cpuspeed} MHz @ ${_cpuperf}${_rset}${pipe}"
+
+	#_cpuspeed=$(printf "%4s" "$(sysctl -n hw.cpuspeed)")
+	_cpuperf=$(printf "%4s" "$(sysctl -n hw.setperf)%")
+
+	#echo -n " ${_grey}${_cpuspeed} MHz @ ${_cpuperf}${_rset}${pipe}"
+	echo -n " ${_grey}@ ${_cpuperf}${_rset}${pipe}"
+}
+
+fan() {
+	_fanspeed=$(sysctl -n hw.sensors.acpithinkpad0.fan0)
+	echo -n "${_grey}${_fanspeed}${pipe}"
 }
 
 group() {
 	echo -n "${_grey}$(xprop -root 32c '\t$0' _NET_CURRENT_DESKTOP | cut -f 2)"
 }
 
-#kbd() {
-	#_kbd=$(setxkbmap -query | awk '/^layout:/ { print $2}')
-	#[[ "${_kbd}" = "de" ]] \
-		#&& echo -n "${_rset}${_good}${_kbd}${pipe}" \
-		#|| echo -n "${_rset}${_crit}${_kbd}${pipe}"
-#}
+kbd() {
+	_kbd=$(setxkbmap -query | awk '/^layout:/ { print $2}')
+	[[ "${_kbd}" = "de" ]] \
+		&& echo -n "${_rset}${_good}${_kbd}${pipe}" \
+		|| echo -n "${_rset}${_crit}${_kbd}${pipe}"
+}
 
 load() {
 	_sysload=$(systat -b | awk 'NR==3 { print $4 }')
-	if [[ "${_sysload}" > "4.00" ]] ; then
+	if [[ "0${_sysload}" > "10.00" ]] ; then
+		echo -n "${_alrt}${_sysload}${pipe}"
+	elif [[ "0${_sysload}" > "04.00" ]] ; then
 		echo -n "${_crit}${_sysload}${pipe}"
-	elif [[ "${_sysload}" > "2.00" ]] ; then
+	elif [[ "0${_sysload}" > "02.00" ]] ; then
 		echo -n "${_warn}${_sysload}${pipe}"
 	else
 		echo -n "${_good}${_sysload}${pipe}"
@@ -84,7 +98,7 @@ load() {
 }
 
 memory() {
-	_mem=$(printf "%5s" "$(top -n | awk -F "( |/)" 'NR == 7 { print $3 }')")
+	_mem=$(printf "%5s" "$(top -n | awk -F "( |/)" '/Memory:/ { print $3 }')")
 	if [[ "0${_mem}" > "10000M" ]] ; then
 		echo -n "${_crit}${_mem}${pipe}"
 	elif [[ "0${_mem}" > "5000M" ]] ; then
@@ -114,16 +128,16 @@ network() {
 			_hublanup=$(ifconfig "${nic[2]}" | grep -c UP)
 			_hublanstat=$(ifconfig "${nic[2]}" | grep -c inet)
 			[[ ${_hublanup} -eq 1 && ${_hublanstat} -gt 0 ]] \
-				&& _netstate="${net[1]}${nic[0]} ${nic[1]}${net[0]} ${nic[2]}" \
+				&& _netstate="${net[1]}${nic[0]} ${nic[1]}${_rset}${net[0]} ${nic[2]}" \
 				|| _netstate="${net[2]}no network"
 		fi
 	else
 		[[ ${_lanstat} -eq 1 ]] \
-			&& _lanstate="${net[0]}${nic[0]} " \
-			|| _lanstate="${net[1]}${nic[0]} "
+			&& _lanstate="${net[0]}${nic[0]}${_rset} " \
+			|| _lanstate="${net[1]}${nic[0]}${_rset} "
 		[[ ${_wlanstat} -eq 1 ]] \
-			&& _wlanstate="${net[0]}${_wlan}" \
-			|| _wlanstate="${net[1]}${nic[1]}"
+			&& _wlanstate="${net[0]}${_wlan}${_rset}" \
+			|| _wlanstate="${net[1]}${nic[1]}${_rset}"
 		if [[ ${_hublanexist} -eq 1 ]] ; then
 			_hublanup=$(ifconfig "${nic[2]}" | grep -c UP)
 			_hublanstat=$(ifconfig "${nic[2]}" | grep -c inet)
@@ -147,14 +161,10 @@ snapshot() {
 }
 
 tasks() {
-	_today=$(grep -c due:"$(date +%Y-%m-%d)" "$TODODIR"/todo.txt)
-	[[ ${_today} != 0 ]] \
-		&& echo -n "${_crit}${_today} " \
-		|| echo -n "${_hide}${_today} "
-	_urgent=$(grep -c ' +urgent' "$TODODIR"/todo.txt)
-	[[ ${_urgent} != 0 ]] \
-		&& echo -n "${_back}${_crit}${_urgent}${pipe}" \
-		|| echo -n "${pipe}"
+	_todo=$(grep -c -e ' +urgent' -e due:"$(date +%Y-%m-%d)" "$TODODIR"/todo.txt)
+	[[ ${_todo} -gt 0 ]] \
+		&& echo -n "${_crit}${_todo}${pipe}" \
+		|| echo -n "${_hide}${_todo}${pipe}"
 }
 
 volume() {
@@ -167,16 +177,14 @@ volume() {
 	#_imute=$(sndioctl -n input.mute)
 	#[[ ${_imute} -eq 1 ]] \
 		#&& echo -n "${_crit}mic on "
-	[[ ${_omute} -eq 1 ]] \
-		&& echo -n "${_rset}${vol[${_omute}]}${_volume}${pipe}" \
-		|| echo -n "${_rset}${vol[${_omute}]}${_volume}${pipe}"
+	echo -n "${_rset}${vol[${_omute}]}${_volume}${pipe}"
 }
 
-#window() {
-	#_wid=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)
-	#_win=$(xprop -id "${_wid}" '\t$0' _NET_WM_NAME | cut -d '"' -f 2)
-	#echo -n "${_grey}${_win}${_back}"
-#}
+window() {
+	_wid=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)
+	_win=$(xprop -id "${_wid}" '\t$0' _NET_WM_NAME | cut -d '"' -f 2)
+	echo -n "${_grey}${_win}${_back}"
+}
 
 tput civis	# hide cursor
 
@@ -184,12 +192,13 @@ while true; do
 	#tput clear cup 1 0
 	tput cup 1 0
 	_l=" $(calendar) $(tasks) $(network) $(battery) $(music)"
-	_r="$(volume) $(cpu) $(memory) $(load) $(snapshot) $(group)"
-	printf "%-319.319s\r" "$_l"
-	#printf "%-329.329s\r" "$_l"
+	printf "%-300.300s\r" "$_l"
 	tput cup 1 142
-	printf "%181.181s" "$_r"
-	sleep 1
+	#_r="$(volume) $(cpu) $(memory) $(load) $(snapshot) $(group)"
+	#printf "%181.181s" "$_r"
+	_r="$(volume) $(cpu) $(fan) $(load) $(snapshot) $(group)"
+	printf "%185.185s" "$_r"
+	sleep 5
 done
 
 tput cnorm	# show cursor
